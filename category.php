c054cfa95591528211b2236292caf4fe0ab236d5
Feliks N <feliks.nvk@gmail.com>
2025-04-07 18:22:34 +0200

    Wrap a category content in a container

--- category.php
+++ category.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
-<h1><?php single_cat_title(); ?></h1>
-<?php get_template_part('loop'); ?>
+<div class="container">
+    <h1><?php single_cat_title(); ?></h1>
+    <?php get_template_part('loop'); ?>
+</div>
 
 <?php get_footer(); ?>
