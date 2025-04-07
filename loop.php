fed4881d5bcecf4e6712c2553c00342bb94874b0
Feliks N <feliks.nvk@gmail.com>
2025-04-07 18:26:26 +0200

    Wrap a posts loop in a bootstrap class

--- loop.php
+++ loop.php
@@ -1,11 +1,10 @@
-<?php if ( have_posts() ) { ?>
-	<?php while ( have_posts() ) { ?>
-		<?php the_post(); ?>
-		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-			<?php the_content(); ?>
-		</div>
+<div class="row row-cols-4">
+	<?php if ( have_posts() ) { ?>
+		<?php while ( have_posts() ) { ?>
+			<?php the_post(); ?>
+		
+		<?php } ?>
+	<?php } else { ?>
+		<p>Wpisów nie odnaleziono!</p>;
 	<?php } ?>
-<?php } else { ?>
-	<p>Wpisów nie odnaleziono!</p>;
-<?php } ?>
+</div>
