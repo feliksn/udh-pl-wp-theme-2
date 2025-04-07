1cacae687a68ed5ac888a625d011fb8f5cf1f592
Feliks N <feliks.nvk@gmail.com>
2025-04-07 18:30:08 +0200

    Add html code to show post data in the posts loop

--- loop.php
+++ loop.php
@@ -2,6 +2,7 @@
 	<?php if ( have_posts() ) { ?>
 		<?php while ( have_posts() ) { ?>
 			<?php the_post(); ?>
+			
 			<?php
 				$parsed_content = PARSER->load( get_the_content() );
 				$tbody_rows = $parsed_content->find('#brand_images tbody tr');
@@ -15,6 +16,19 @@
 					$post_img_url = wp_get_attachment_image_url($post_img_id, 'medium');
 				}
 			?>
+			
+			<!-- Post in the loop -->
+			<div id="post-<?php the_ID(); ?>" <?php post_class('col'); ?>>
+				<a href="<?php the_permalink(); ?>">
+					<img class="img-fluid" src="<?php echo $post_img_url ?>" alt="">
+				</a>
+				<div class="p-3">
+					<a class="d-block mb-3" href="<?php the_permalink(); ?>">
+						<img src="<?php echo $logo_img_url ?>" alt="">
+					</a>
+					<?php the_excerpt(); ?>
+				</div>
+			</div>
 
 		<?php } ?>
 	<?php } else { ?>
