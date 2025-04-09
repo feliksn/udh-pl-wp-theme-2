172615d26a32c380a4f469db1ae3db6cfa5c69a1
Feliks N <feliks.nvk@gmail.com>
2025-04-09 14:51:51 +0200

    Create a custom post type query and pass the query to a loop temlpate

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
-<h1>Strona główna</h1>
-<?php get_template_part('loop'); ?>
+<div class="container">
+    <?php $query = new WP_Query('post_type=brand'); ?>
+    <?php get_template_part('loop', null, $query); ?>
+</div>
 
 <?php get_footer(); ?>
