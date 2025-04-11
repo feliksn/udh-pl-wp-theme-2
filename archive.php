bd1f825f0189aa2a8baa6a8bf4a7bd9a1a8b86c2
Feliks N <feliks.nvk@gmail.com>
2025-04-11 11:22:58 +0200

    Move an archive content in a container

--- archive.php
+++ archive.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
-<h1>Archiwum</h1>
-<?php get_template_part('loop'); ?>
+<div class="container">
+    <h1>Archiwum</h1>
+    <?php get_template_part('loop'); ?>
+</div>
 
 <?php get_footer(); ?>
