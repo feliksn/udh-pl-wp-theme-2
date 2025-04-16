b02dc4cc94bf23e5a35716089d2831e0ea94150e
Feliks N <feliks.nvk@gmail.com>
2025-04-16 15:15:39 +0200

    Use a $_post var to display post data

--- loop.php
+++ loop.php
@@ -8,23 +8,34 @@
 			<!-- getPost() checks which current post type is in the loop -->
 			<!-- Don't use $post!!! It is a global var. It could cause an error; -->
 			<?php $_post = getPost(); ?>
-			
-			<!-- Post in the loop -->
-			<div id="post-<?php the_ID(); ?>" <?php post_class('col'); ?>>
-				<a href="<?php the_permalink(); ?>">
-					<img class="w-100" src="<?php echo $post_img_url ?>" alt="">
-				</a>
-				<div class="p-3">
-					<a class="d-block mb-3" href="<?php the_permalink(); ?>">
-						<img src="<?php echo $logo_img_url ?>" alt="">
-					</a>
-					<?php echo $description; ?>
-				</div>
-			</div>
 
+			<!-- A brand in a loop -->
+			<div id="post-<?php the_ID(); ?>" <?php post_class('col'); ?>>
+				
+				<!-- A brand image as a link -->
+				<a href="<?php the_permalink(); ?>">
+					<img class="w-100" src="<?php echo $_post['image_url'] ?>" alt="">
+				</a>
+				
+				<div class="p-3">
+					<!-- A brand name if a brand logo doesn't exist -->
+					<a class="d-block mb-3" href="<?php the_permalink(); ?>">
+						<?php if( isset( $_post['logo_url'] ) ){ ?>
+							<img src="<?php echo $_post['logo_url'] ?>" alt="">
+						<?php } else { ?>
+							<h2><?php the_title(); ?></h2>
+						<?php } ?> 
+					</a>
+					<!-- A brand description -->
+					<?php echo $_post['description']; ?>
+				</div>
+
+			</div>
 		<?php } ?>
 	<?php } else { ?>
-		<p>Wpisów nie odnaleziono!</p>
+		
+		<h4>Wpisów nie odnaleziono!</h4>
+	
 	<?php } ?>
 	<?php wp_reset_postdata(); ?>
 </div>
